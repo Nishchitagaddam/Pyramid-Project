5c5fa5fbb27a2df9b3c8b3bef57dea0f074445fc
priyavemasani <Tejakoppuravuri8@gmail.com>
2017-01-07 21:26:25 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,63 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
 
 requires = [
     'pyramid',
     'pyramid_chameleon',
-]
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
 
-setup(name='student',
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+setup(name='whee',
+      version='0.0',
+      description='whee',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      
+         #...
+         install_requires=[
+             'pyramid_mako',
+             'pyramid',
+             #...
+         ],
+         #...
+         install_requires=[
+             'pyramid_chameleon',
+             'pyramid',
+             #...
+          ],
+       
       install_requires=requires,
       entry_points="""\
       [paste.app_factory]
-      main = student:main
+      main = whee:main
       """,
-)
+      )
