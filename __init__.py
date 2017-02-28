c138e80764f9081ed3b9d61816870a236b5d8abc
Nishchitagaddam <gnishchita@gmail.com>
2017-02-28 17:54:58 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
+    config.add_route('wiki_view', '/')
+    config.add_route('wikipage_add', '/add')
+    config.add_route('wikipage_view', '/{uid}')
+    config.add_route('wikipage_edit', '/{uid}/edit')
+    config.add_static_view('deform_static', 'deform:static/')
+    config.scan('.views')
     return config.make_wsgi_app()
